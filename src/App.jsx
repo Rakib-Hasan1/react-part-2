fed8b6b3b4b053e122fdce3e5871f230682640cc
rakib hasan <dev.rakibhasan1@gmail.com>
2025-03-24 02:49:22 +0600

    added fetch and some calculating function

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,71 @@
+import "./App.css";
+import Counter from "./counter";
+import Batsman from "./Batsman";
+// import Users from "./summary";
+import { Suspense } from "react";
+import Friends from "./friends";
 
-import './App.css'
-import Counter from './counter';
-import Batsman from './Batsman';
+// const fetchUsers = fetch("https://jsonplaceholder.typicode.com/users").then(
+//   (res) => res.json());
+
+
+  const fetchFriends = async() => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users')
+    return res.json(); 
+  }
+
 
 
 function App() {
 
+
+  const friendsPromise = fetchFriends();
+
+
+
+
+
   const handleAlert = () => {
-    alert("I am Clicked")
-  }
+    alert("I am Clicked");
+  };
 
-  function handle2 (num) {
+  function handle2(num) {
     const newNumber = num + 20;
-    alert(newNumber)
+    alert(newNumber);
   }
-
 
   return (
     <>
+      {/* <Suspense fallback={<h3>Loading ...</h3>}>
+          <Users fetchUsers={fetchUsers}></Users>
+      </Suspense> */}
 
 
 
-<Batsman></Batsman>
+      <Suspense fallback={<h3>Friends are comming for treats...</h3>}>
+        <Friends friendsPromise={friendsPromise}></Friends>
+      </Suspense>
 
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
 
+      <Batsman></Batsman>
 
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <Counter></Counter>
 
-<br />
-<br />
-<br />
-<br />
-<br />
-<Counter></Counter>
-
-
-
-
-
-
-
-
-
-
-<button onClick={handleAlert}>Clik Me</button>
-<button onClick={()=>handle2(2)}>Click Me 2</button>
-
-
-
-
-
+      <button onClick={handleAlert}>Clik Me</button>
+      <button onClick={() => handle2(2)}>Click Me 2</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
